bb09696659e7b9551d2bd4a1069907d5cf05de49
Pedro Molina <pedro.ivo.molina@gmail.com>
2019-06-09 23:03:49 -0300

    Refactored menu

--- src/ui/Menu.java
+++ src/ui/Menu.java
@@ -1,60 +1,35 @@
 package ui;
 
-import java.util.Scanner;
-
 import main.ATM;
 import main.UI;
-import Screen;
-import service.Deposit;
-import service.Transaction;
-import service.Withdrawal;
+import Keypad;
 
 
-public class Menu implements UI {
-  private static final int BALANCE_INQUIRY = 1;
-  private static final int WITHDRAWAL = 2;
-  private static final int DEPOSIT = 3;
-  private static final int EXIT = 4;
-  private Scanner in;
-  
-  // display the main menu and return an input selection
-  private int displayMainMenu()
+public class Menu implements UI 
+{
+  private ATM atm;  
+  private Keypad keypad;
+
+  public Menu()
   {
-    screen.displayMessageLine("\nMain menu:");
-    screen.displayMessageLine("1 - View my balance");
-    screen.displayMessageLine("2 - Withdraw cash");
-    screen.displayMessageLine("3 - Deposit funds");
-    screen.displayMessageLine("4 - Exit\n");
-    screen.displayMessage("Enter a choice: ");
+	atm = new ATM();
+	keypad = new Keypad();
+  }
+  
+  public int menu()
+  {
+    System.out.println("\nMain menu:");
+    System.out.println("1 - View my balance");
+    System.out.println("2 - Withdraw cash");
+    System.out.println("3 - Deposit funds");
+    System.out.println("4 - Exit\n");
+    System.out.println("Enter a choice: ");
     return keypad.getInput();
   }
-
-  // return object of specified Transaction subclass
+  
   private Transaction createTransaction(int type)
   {
-     Transaction temp = null; // temporary Transaction variable
-     
-     // determine which type of Transaction to create     
-     switch (type)
-     {
-        case BALANCE_INQUIRY: // create new BalanceInquiry transaction
-           temp = new BalanceInquiry(
-              currentAccountNumber, screen, bankDatabase);
-           break;
-        case WITHDRAWAL: // create new Withdrawal transaction
-           temp = new Withdrawal(currentAccountNumber, screen, 
-              bankDatabase, keypad, cashDispenser);
-           break; 
-        case DEPOSIT: // create new Deposit transaction
-           temp = new Deposit(currentAccountNumber, screen, 
-              bankDatabase, keypad, depositSlot);
-           break;
-         case EXIT:
-            screen.displayMessageLine("Exiting");
-            break;
-     } // end switch
-
-     return temp; // return the newly created object
-  } // end method createTransaction
+	 atm.executeTransaction(type);
+  }
 }
 
