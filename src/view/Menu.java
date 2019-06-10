3a09b5ec977c8bfc53f9cadc367bb1479514fbfc
Pedro Molina <pedro.ivo.molina@gmail.com>
2019-06-10 00:12:49 -0300

    Fiz o menu ficar top

--- src/view/Menu.java
+++ src/view/Menu.java
@@ -5,19 +5,17 @@ import main.UI;
 import Keypad;
 
 
-public class Menu implements UI 
-{
+public class Menu implements UI {
   private ATM atm;  
   private Keypad keypad;
 
-  public Menu()
-  {
-	atm = new ATM();
-	keypad = new Keypad();
+  public Menu(){
+	  atm = new ATM();
+	  keypad = new Keypad();
   }
   
-  public int menu()
-  {
+  public int menu(){
+    authenticate();
     System.out.println("\nMain menu:");
     System.out.println("1 - View my balance");
     System.out.println("2 - Withdraw cash");
@@ -27,19 +25,22 @@ public class Menu implements UI
     return keypad.getInput();
   }
   
-  private Transaction createTransaction(int type)
-  {
-	 atm.executeTransaction(type);
+  private Transaction createTransaction(int type){
+	  atm.executeTransaction(type);
   }
 
-  /** NÃO ESQUECER DE FAZER UM MÉTODO QUE VAI PEGAR ESSA PORRA!!!!!!!!!!!!!!!!!!!!!!!!!!
-  System.out.displayMessage("\nPlease enter your account number: ");
-  int accountNumber = keypad.getInput();
-  screen.displayMessage("\nEnter your PIN: ");
-  int pin = keypad.getInput();
+  public void authenticate(){       
+    System.out.println("\nPlease enter your account number: ");
+    int accountNumber = keypad.getInput();
+    System.out.println("\nEnter your PIN: ");
+    int pin = keypad.getInput();
 
-  screen.displayMessageLine("\nWelcome!");       
-  authenticateUser(); // authenticate user
-  */
+    boolean authenticated = atm.authenticateUser(accountNumber, pin);
+
+    if (!authenticated){
+      System.out.println("\nError!\n You must enter a valid account number and PIN!");
+      authenticate();
+    }
+  }
 }
 
